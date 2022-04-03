e0bbc787a1dcbc5a8456dced4c4b95acebf81050
chiangkd <chiangkuandi@gmail.com>
2022-04-03 21:40:56 +0800

    q1 code tracing

---
+++ quiz1/q1.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+static int cmp(const void *lhs, const void *rhs) {
+    if (*(int *) lhs == *(int *) rhs)
+        return 0;
+    return *(int *) lhs < *(int *) rhs ? -1 : 1;
+}
+
+static int *alloc_wrapper(int a, int b, int *returnSize) {
+    *returnSize = 2;
+    int *res = (int *) malloc(sizeof(int) * 2);
+    res[0] = a, res[1] = b;
+    return res;
+}
+
+int *twoSum(int *nums, int numsSize, int target, int *returnSize)
+{
+    *returnSize = 2;
+    int arr[numsSize][2];  /* {value, index} pair */
+    for (int i = 0; i < numsSize; ++i) {
+        arr[i][0] = nums[i];
+        arr[i][1] = i;
+    }
+    qsort(arr, numsSize, sizeof(arr[0]), cmp);
+    for (int i = 0, j = numsSize - 1; i < j; ) {
+        if (arr[i][0] + arr[j][0] == target)
+            return alloc_wrapper(arr[i][1], arr[j][1], returnSize);
+        if (arr[i][0] + arr[j][0] < target)
+            ++i;
+        else
+            --j;
+    }
+    *returnSize = 0;
+    return NULL;
+}      
+
+int main() {
+    int nums[4] = {2, 11, 7, 15};
+    int numsSize = 4;
+    int *returnSize ;
+    int target = 9;
+
+    int* output = twoSum(nums, numsSize, target, returnSize);
+    
+    for (int i = 0; i < 2; i++) {
+        printf("%d\t",output[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
---
+++ quiz1/q1_hash.c
@@ -0,0 +1,132 @@
+#include <stddef.h>
+#include <stdlib.h>
+
+struct hlist_node { struct hlist_node *next, **pprev; };
+struct hlist_head { struct hlist_node *first; };
+typedef struct { int bits; struct hlist_head *ht; } map_t;
+
+#define MAP_HASH_SIZE(bits) (1 << bits)
+
+map_t *map_init(int bits) {
+    map_t *map = malloc(sizeof(map_t));
+    if (!map)
+        return NULL;
+
+    map->bits = bits;
+    map->ht = malloc(sizeof(struct hlist_head) * MAP_HASH_SIZE(map->bits));
+    if (map->ht) {
+        for (int i = 0; i < MAP_HASH_SIZE(map->bits); i++)
+            (map->ht)[i].first = NULL;
+    } else {
+        free(map);
+        map = NULL;
+    }
+    return map;
+}
+
+struct hash_key {
+    int key;
+    void *data;
+    struct hlist_node node;
+};
+
+#define container_of(ptr, type, member)               \
+    ({                                                \
+        void *__mptr = (void *) (ptr);                \
+        ((type *) (__mptr - offsetof(type, member))); \
+    })
+
+#define GOLDEN_RATIO_32 0x61C88647
+static inline unsigned int hash(unsigned int val, unsigned int bits) {
+    /* High bits are more random, so use them. */
+    return (val * GOLDEN_RATIO_32) >> (32 - bits);
+}
+
+static struct hash_key *find_key(map_t *map, int key) {
+    struct hlist_head *head = &(map->ht)[hash(key, map->bits)];
+    for (struct hlist_node *p = head->first; p; p = p->next) {
+        struct hash_key *kn = container_of(p, struct hash_key, node);
+        if (kn->key == key)
+            return kn;
+    }
+    return NULL;
+}
+
+void *map_get(map_t *map, int key)
+{
+    struct hash_key *kn = find_key(map, key);
+    return kn ? kn->data : NULL;
+}
+
+void map_add(map_t *map, int key, void *data)
+{
+    struct hash_key *kn = find_key(map, key);
+    if (kn)
+        return;
+
+    kn = malloc(sizeof(struct hash_key));
+    kn->key = key, kn->data = data;
+
+    struct hlist_head *h = &map->ht[hash(key, map->bits)];
+    struct hlist_node *n = &kn->node, *first = h->first;
+    AAA;
+    if (first)
+        first->pprev = &n->next;
+    h->first = n;
+    BBB;
+}
+
+void map_deinit(map_t *map)
+{
+    if (!map)
+        return;
+
+    for (int i = 0; i < MAP_HASH_SIZE(map->bits); i++) {
+        struct hlist_head *head = &map->ht[i];
+        for (struct hlist_node *p = head->first; p;) {
+            struct hash_key *kn = container_of(p, struct hash_key, node);
+            struct hlist_node *n = p;
+            p = p->next;
+
+            if (!n->pprev) /* unhashed */
+                goto bail;
+
+            struct hlist_node *next = n->next, **pprev = n->pprev;
+            *pprev = next;
+            if (next)
+                next->pprev = pprev;
+            n->next = NULL, n->pprev = NULL;
+
+        bail:
+            free(kn->data);
+            free(kn);
+        }
+    }
+    free(map);
+}
+
+int *twoSum(int *nums, int numsSize, int target, int *returnSize)
+{
+    map_t *map = map_init(10);
+    *returnSize = 0;
+    int *ret = malloc(sizeof(int) * 2);
+    if (!ret)
+        goto bail;
+
+    for (int i = 0; i < numsSize; i++) {
+        int *p = map_get(map, target - nums[i]);
+        if (p) { /* found */
+            ret[0] = i, ret[1] = *p;
+            *returnSize = 2;
+            break;
+        }
+
+        p = malloc(sizeof(int));
+        *p = i;
+        map_add(map, nums[i], p);
+    }
+
+bail:
+    map_deinit(map);
+    return ret;
+}
