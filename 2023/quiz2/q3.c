f727bf0bac535f78423ac8017946e71686cce532
chiangkd <chiangkuandi@gmail.com>
2023-03-08 04:39:04 +0800

    Add test performance code

--- 2023/quiz2/q3.c
+++ 2023/quiz2/q3.c
@@ -2,6 +2,12 @@
 #include <stddef.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "cpucycles.h"
+
+/* Prevent GCC to optimize out the body of count_utf8 and swar_count_utf8 */
+static volatile size_t _count;
 
 size_t count_utf8(const char *buf, size_t len)
 {
@@ -38,7 +44,43 @@ size_t swar_count_utf8(const char *buf, size_t len)
     return count;
 }
 
-int main()
+
+#define SBUFSIZE 4
+#define NTESTS 10
+
+typedef size_t (*fptr_countf8)(const char *, size_t);
+
+uint32_t performance(fptr_countf8 operation, const char *buf, size_t len)
 {
-    ;
+    size_t ans1, ans2;
+    uint32_t tick1, tick2;
+    /* start test */
+    tick1 = cpucycles();
+    ans1 = operation(buf, len);
+    tick2 = cpucycles();
+
+    return tick2 - tick1;
+}
+
+int main(void)
+{
+    int seed;
+    seed = (int)time(NULL);
+    srand(seed);
+    int bufsize[SBUFSIZE] = {10000, 20000, 50000, 100000};
+    
+    uint32_t count_no_swar=0, count_with_swar=0;
+    for (int i = 0; i < SBUFSIZE; i++) {
+        uint8_t buf[bufsize[i]];   // utf-8 sequence
+        for(int j = 0; j < NTESTS; j++) {
+            for (int k = 0; k < bufsize[i]; k++)
+                buf[k] = rand();
+        count_no_swar += performance(count_utf8, buf, bufsize[i]);
+        count_with_swar += performance(swar_count_utf8, buf, bufsize[i]);
+    }
+    printf("%d\t %d\t %d\n",bufsize[i] ,count_no_swar/NTESTS, count_with_swar/NTESTS);
+    }
+    
+
+    return 0;
 }
